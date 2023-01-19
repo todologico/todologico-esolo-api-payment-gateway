cc940a278cb11c6b266efa68792b9b4f3ab3891d
Rendo <arturodiehl@gmail.com>
2023-01-19 16:21:23 -0300

    Gateway Interface for decoupling payment providers

---
+++ apipaylar/app/Interfaces/GatewayInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\AppInterfaces;
+use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
+use Auth;
+
+
+//----------------------------------------------------------
+// PAYMENT INTERFACE
+//----------------------------------------------------------
+// Used by : 
+//----------------------------------------------------------
+
+//----------------------------------------------------------
+
+interface Payment_Interface
+{
+    public function payNow();
+}
+//----------------------------------------------------------
